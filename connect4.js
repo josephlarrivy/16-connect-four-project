ccc64895c775a3a4a56420caa33fe45193e06eb7
josephlarrivy <joseph.larrivy@gmail.com>
2022-08-28 21:23:19 -0500

    making progress

--- connect4.js
+++ connect4.js
@@ -11,60 +11,59 @@ const HEIGHT = 6;
 let currPlayer = 1; // active player: 1 or 2
 let board = []; // array of rows, each row is array of cells  (board[y][x])
 
-
-
-
-
-
-
-
-
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
-// TODO: set "board" to empty HEIGHT x WIDTH matrix array
+// TODO(done): set "board" to empty HEIGHT x WIDTH matrix array
 function makeBoard() {
-  for (let i=0; i< HEIGHT; i++);
-    for (let j=0; j< WIDTH; j++)
-      board.push([x,y]);
+  let boardRows = [];
+  for (let i=0; i<WIDTH; i++) {
+    boardRows.push([null]);
+  }
+  for (let j=0; j<HEIGHT; j++) {
+    board.push(boardRows);
+  }
 }
 
-makeBoard();
-console.log(board);
-
-
-
-
-
-
-
-
 /** makeHtmlBoard: make HTML table and row of column tops. */
-
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+  // TODO (done): get "htmlBoard" variable from the item in HTML w/ID of "board"
+  let htmlBoard = document.getElementById('board');
 
-  // TODO: add comment for this code
+  // TODO(done): add comment for this code
   let top = document.createElement("tr");
+  //JS variable of 'top' is declared with a value of the table row
   top.setAttribute("id", "column-top");
+  //new variable 'top' is given the id of 'column-tp'
   top.addEventListener("click", handleClick);
+  //the 'top' variable gets an event listener of 'click' and the function 'handleClick' executes when the event occurs
 
   for (let x = 0; x < WIDTH; x++) {
+    //we loop the number of times that the board is wide
     let headCell = document.createElement("td");
+    //on each loop, we create a table data element called 'headCell'
     headCell.setAttribute("id", x);
+    //headCell is assigned an id of 'x'
     top.append(headCell);
+    //each headCell is appended (given a child) to top
   }
   htmlBoard.append(top);
 
   // TODO: add comment for this code
   for (let y = 0; y < HEIGHT; y++) {
+    //we loop the number of times that the board is tall
     const row = document.createElement("tr");
+    //a variable of 'row' is arrigned to each table-row that is created
     for (let x = 0; x < WIDTH; x++) {
       const cell = document.createElement("td");
+      //an inner loop loops over the previously created table-row and create a table-data element each time
       cell.setAttribute("id", `${y}-${x}`);
+      //an id containing the x and y values of the two loops is assigned to each table-data that is created
       row.append(cell);
+      //each row is appended with the new 'cell'
     }
     htmlBoard.append(row);
+    //the html board is appended with the new rows(and each row's cells)
   }
 }
 
@@ -79,12 +78,25 @@ function findSpotForCol(x) {
 
 function placeInTable(y, x) {
   // TODO: make a div and insert into correct table cell
+  const checker = document.createElement('div');
+  checker.classList.add('piece');
+  checker.classList.add(`player${currPlayer}Piece`);
+  // TODO: add the div to the correct td cell in the HTML game board (STEP Five in instructions)
+  
+
 }
+
+
+
+
+
+
 
 /** endGame: announce game end */
 
 function endGame(msg) {
-  // TODO: pop up alert message
+  // TODO(done): pop up alert message
+  setTimeout(alert(`Player ${currPlayer} wins!`), 200)
 }
 
 /** handleClick: handle click of column top to play piece */
@@ -110,9 +122,29 @@ function handleClick(evt) {
 
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
+  function checkForTie() {
+    board.every(function(){
+      
+    })
+  }
+
+  if (checkForTie()) {
+    return endGame(`The game is a tie`);
+  }
+
+
+
+
+
+
+
+
+
+
 
   // switch players
-  // TODO: switch currPlayer 1 <-> 2
+  // TODO(done): switch currPlayer 1 <-> 2
+  currPlayer = currPlayer = 1 ? 2 : 1;
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
