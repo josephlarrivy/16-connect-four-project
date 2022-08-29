a59c935d28a027ea6e13eb8dc1e2775c2767969f
josephlarrivy <joseph.larrivy@gmail.com>
2022-08-29 12:38:42 -0500

    exercise complete - still need to add more notes

--- connect4.js
+++ connect4.js
@@ -15,20 +15,32 @@ let board = []; // array of rows, each row is array of cells  (board[y][x])
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 // TODO(done): set "board" to empty HEIGHT x WIDTH matrix array
+
+// function makeBoard() {
+//   let boardRows = [];
+//   for (let i=0; i<WIDTH; i++) {
+//     boardRows.push([null]);
+//   }
+//   for (let j=0; j<HEIGHT; j++) {
+//     board.push(boardRows);
+//   }
+// }
 function makeBoard() {
-  let boardRows = [];
-  for (let i=0; i<WIDTH; i++) {
-    boardRows.push([null]);
-  }
-  for (let j=0; j<HEIGHT; j++) {
-    board.push(boardRows);
+  for (let y=0; y<HEIGHT; y++) {
+    board.push(Array.from({length: WIDTH}));
   }
 }
+//is this code^^ object-oriented programming?
+
+
+
+
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 function makeHtmlBoard() {
   // TODO (done): get "htmlBoard" variable from the item in HTML w/ID of "board"
-  let htmlBoard = document.getElementById('board');
+  //JS variable is set by getting HTML element 'board' from index.html
+  let board = document.getElementById('board');
 
   // TODO(done): add comment for this code
   let top = document.createElement("tr");
@@ -47,7 +59,7 @@ function makeHtmlBoard() {
     top.append(headCell);
     //each headCell is appended (given a child) to top
   }
-  htmlBoard.append(top);
+  board.append(top);
 
   // TODO(done): add comment for this code
   for (let y = 0; y < HEIGHT; y++) {
@@ -62,7 +74,7 @@ function makeHtmlBoard() {
       row.append(cell);
       //each row is appended with the new 'cell'
     }
-    htmlBoard.append(row);
+    board.append(row);
     //the html board is appended with the new rows(and each row's cells)
   }
 }
@@ -72,7 +84,7 @@ function makeHtmlBoard() {
 function findSpotForCol(x) {
   // TODO(done): write the real version of this, rather than always returning 0
   //this function returns the height where the next piece should drop down to
-  for (let y=HEIGHT-1; y>=0; y--) {
+  for (let y = HEIGHT-1; y>=0; y--) {
       if (!board[y][x]) {
         return y;
       }
@@ -80,34 +92,25 @@ function findSpotForCol(x) {
   return null;
 }
 
-
-
-
-
-
-
-
-
-
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
   // TODO(done): make a div and insert into correct table cell
-  const checker = document.createElement('div');
-  checker.classList.add('piece');
-  checker.classList.add(`player${currPlayer}Piece`);
+  const piece = document.createElement('div');
+  piece.classList.add('piece');
+  piece.classList.add(`player${currPlayer}Piece`);
   // TODO(done): add the div to the correct td cell in the HTML game board (STEP Five in instructions)
   
   let place = document.getElementById(`${y}-${x}`);
 
-  place.append(checker);
+  place.append(piece);
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
   // TODO(done): pop up alert message
-  setTimeout(alert(msg), 200)
+  alert(msg);
 }
 
 /** handleClick: handle click of column top to play piece */
@@ -130,13 +133,8 @@ function handleClick(evt) {
 
   // place piece in board and add to HTML table
   // TODO(done): add line to update in-memory board
-  currPlayer = board[y][x];
+  board[y][x] = currPlayer;
   placeInTable(y, x);
-
-
-
-
-
 
   // check for win
   if (checkForWin()) {
@@ -163,9 +161,6 @@ function handleClick(evt) {
 
 function checkForWin() {
   function _win(cells) {
-    // Check four cells to see if they're all color of current player
-    //  - cells: list of four (y, x) cells
-    //  - returns true if all are legal coordinates & all match currPlayer
 
     return cells.every(
       ([y, x]) =>
@@ -177,15 +172,12 @@ function checkForWin() {
     );
   }
 
-  // TODO: read and understand this code. Add comments to help you.
-
   for (let y = 0; y < HEIGHT; y++) {
     for (let x = 0; x < WIDTH; x++) {
       const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
       const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
       const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
       const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
-
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
       }
